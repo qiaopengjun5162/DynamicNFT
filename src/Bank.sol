74d45351adf96e0b4dd64317bf369343956e6c37
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-08-11 16:02:03 +0800

    style: fmt

--- src/Bank.sol
+++ src/Bank.sol
@@ -32,23 +32,21 @@ contract Bank is AutomationCompatibleInterface {
     }
 
     // checkUpKeep()：在链下间隔执行调用该函数， 该方法返回一个布尔值，告诉网络是否需要自动化执行。
-    function checkUpkeep(
-        bytes calldata /* checkData */
-    )
+    function checkUpkeep(bytes calldata /* checkData */ )
         external
         view
         override
-        returns (bool shouldTransferFunds, bytes memory /* performData */)
+        returns (bool shouldTransferFunds, bytes memory /* performData */ )
     {
         shouldTransferFunds = (address(this).balance > threshold);
     }
 
     // performUpKeep()：这个方法接受从checkUpKeep()方法返回的信息作为参数。Chainlink Automation 会触发对它的调用。函数应该先进行一些检查，再执行链上其他计算。
-    function performUpkeep(bytes calldata /* performData */) external override {
+    function performUpkeep(bytes calldata /* performData */ ) external override {
         if (address(this).balance > threshold) {
             uint256 halfBalance = address(this).balance / 2;
             // payable(owner).transfer(halfBalance);
-            (bool success, ) = payable(owner).call{value: halfBalance}("");
+            (bool success,) = payable(owner).call{value: halfBalance}("");
             require(success, "Transfer failed");
         }
     }
